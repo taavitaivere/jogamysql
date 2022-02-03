3f50358b32fdbb1ba476c24c3177303ceaea66d0
Taavi taivere <taavi.taivere@khk.ee>
2022-02-03 14:05:00 +0200

    Added static css and image files

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express()
 const pats = require('path')
 const mysql = require('mysql')
 
+const hbs = require('express-handlebars');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+    extname:'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts',
+}))
+app.use(express.static('public'));
+
+const mysql = require('mysql')
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extend: true}))
 
