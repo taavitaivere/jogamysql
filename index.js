6d15d1b2b701c2008c9614abef3f82020417c326
Taavi taivere <taavi.taivere@khk.ee>
2022-02-03 15:01:11 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const pats = require('path')
+const path = require('path')
 const mysql = require('mysql')
 
 const hbs = require('express-handlebars');
@@ -14,7 +14,7 @@ app.engine('hbs', hbs.engine({
 }))
 app.use(express.static('public'));
 
-const mysql = require('mysql')
+
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extend: true}))
@@ -30,5 +30,17 @@ con.connect(function(err) {
     if (err) throw err;
     console.log('Connected to joga_mysql db');
 })
+app.get('/', (req, res) => {
+    let query = 'select * from article';
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })
+});
+
 
 app.listen(3000, () => {console.log('App s started at http://localhost:3000'); });
